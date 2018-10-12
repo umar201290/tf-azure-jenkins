81dce5b74fb8a72012df72e712ad32d177486fbc
CORP\mu185029 <mu185029@ncr.com>
2018-10-12 08:42:41 +0530

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "main" {
+  name     = "testing-RG"
+  location = "West US 2"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "testing-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.2.0/24"
+}
